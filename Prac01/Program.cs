097c31a677b63bed64bf63766d6953e92470fbbb
Serhatuzn <seerhatuzun@gmail.com>
2024-11-06 23:46:43 +0300

    Generic Lists Practical

---
+++ Prac01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic; 
+Console.WriteLine("** Davetliler **");
+
+List<string> isim = new List<string>(); // string tipinde bir list tanımlıyoruz. 
+{
+    // Tek tek davetli isimleri burada ekliyoruz
+    isim.Add("1 - Bülent Ersoy");
+    isim.Add("2 - Ajda Pekkan");
+    isim.Add("3 - Ebru Gündeş");
+    isim.Add("4 - Hadise");
+    isim.Add("5 - Hande Yener");
+    isim.Add("6 - Tarkan");
+    isim.Add("7 - Funda Arar");
+    isim.Add("8 - Demet Akalın");
+}
+
+foreach(string dolas in isim) // foreach ile dolaştırıp yazdırıyıoruz.
+{
+    Console.WriteLine(dolas);
+}
---
+++ Prac02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+Console.WriteLine("Lütfen 5 tane kahve ismi giriniz: "); // Kullanıcıdan kahve isimlerini girmesini isteyen mesaj
+
+List<string> Kahve = new List<string>(); // Kahve isimlerini saklayacak bir liste oluşturuluyor
+
+// 5 kahve ismi almak için bir döngü
+for (int i = 1; i < 6; i++) 
+{
+    // Kullanıcıdan her bir kahve ismini almak için konsola yazdırılır
+    Console.Write($"Kahve {i} : "); 
+    Kahve.Add(Console.ReadLine()); // Kullanıcının girdiği her kahve ismini listeye ekler
+}
+
+Console.WriteLine("-----------------------------"); // Görsel ayırıcı çizgi
+
+Console.WriteLine("Girilen Kahve İsimleri:"); // Kullanıcıya girilen kahve isimlerini gösterecek mesaj
+
+// Listeyi yazdırmak için foreach döngüsü kullanılır
+foreach (string Kahveler in Kahve)
+{
+    Console.WriteLine(Kahveler); // Her bir kahve ismini konsola yazdırır
+}
